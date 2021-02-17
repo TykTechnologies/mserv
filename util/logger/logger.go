e9afdc12d9aa1d9cc3323c291231c6948c2a17f1
James Lucktaylor <jlucktay+github@gmail.com>
2021-02-17 12:49:47 +0000

    refactor(util/logger): remove dead/unused code

--- util/logger/logger.go
+++ util/logger/logger.go
@@ -8,15 +8,6 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-var logged = false
-
-func logOnce(log *logrus.Logger) {
-	if logged {
-		return
-	}
-	logged = true
-}
-
 // GetLogger will provide a tagged logger by passing in a 'tag' value for easier log parsing.
 func GetLogger(tag string) *logrus.Entry {
 	lvl := os.Getenv("TYK_MSERV_LOGLEVEL")
